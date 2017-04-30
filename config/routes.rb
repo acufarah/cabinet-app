7eb2a3ce730399bda020ab932f952aed77618ddd
acufarah <farahkhanu@gmail.com>
2017-04-30 12:08:48 -0500

    Styled Welcome Page and styled Docs Show Page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   	root "docs#index", as: "authenticated_root"
   end
 
-  root 'welcome index'
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
